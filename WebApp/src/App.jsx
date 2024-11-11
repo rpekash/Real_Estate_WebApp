082e75ef54ba2d6678b8fa7de99647b99ed7dab7
rpekash <kastrijot72@gmail.com>
2024-11-11 00:13:46 -0500

    Login/Signup/MockDataBase

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,17 +1,22 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx';
+import Home from './Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
-  return(
-    
-        <>
-        <Header />
-        <Footer />
-        </>
-        
-    
-  ); 
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+} 
 
 export default App
